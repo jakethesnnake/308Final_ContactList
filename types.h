4108cb40da1ada48f33f5dc186bb2b37d318cd95
Jake Willson <jakedwillson@cougar.cs.ksu.edu>
2018-11-27 14:20:04 -0600

    Post-lab setup. Classes not yet linked so will not yet compile. I'll link them today or tomrrow.

--- types.h
+++ types.h
@@ -2,32 +2,36 @@
 
 // Both LinkedLists can be changed into dynamic arrays
 // VCARD has not been implemented
+
+// 1 
+
 #ifndef types_h
 #define types_h
 #define max 100
 
 typedef struct Contact
 {
-	struct Contact * next; // LinkedList -- can change
+	struct Contact * next; // LinkedList --> dynamic array
 	char * f_name;
 	char * l_name;
 	char * email;
 	long * phone_num;
+	char * address;
 } Contact;
 
 typedef struct Account
 {
-	struct Account * next;	// LinkedList -- can change
+	struct Account * next;	// LinkedList --> dynamic array
 	char * username;
 	char * password;
 } Account;
 
 typedef struct Command
 {
-	enum { ADD, FIND, DISPLAY, CHANGE, REMOVE} type;
-	union {long phone_num; char * string;} search; // string is all else
+	enum { ADD, FIND, VCARD, CHANGE, REMOVE} type;
+	union { long phone_num; char * string; } search; // string is all else
 } Command;
 
-typedef enum {LOGIN, ACTION, QUIT} State; // Session runtime state
+typedef enum {ACCT_SETUP, LOGIN, ACTION, QUIT} State; // Session runtime state
 
 #endif 
