43b10930e6e2574637abc172a22d68423c275df2
Jake Willson <jakedwillson@ksu.edu>
2018-12-13 13:22:22 -0600

    Update core.c

--- core.c
+++ core.c
@@ -10,28 +10,6 @@
 
 int core_main(int argc, const char * argv[])
 {
+	userIO();
 	
-	// **parse user input
-	// **find if user wants to createAccount(0) or login(1)
-	// **enforce valid input (loops)
-	
-	int choice = 1;
-	char * uname = "John";
-	char * psw = "Doe";
-	
-	// start of while loop
-	if(choice == 1) // login attempt
-	{
-		if(tryLogin(uname, psw)) printf("Welcome back!\n");
-		else printf("Invalid login attempt\n");
-    }
-	else if(choice == 0) // create account attempt
-	{
-		int b = tryCreateAccount(uname,psw);
-		if(b == TRUE) printf("new!\n"); else printf("--old\n");
-	}
-	else printf("Invalid Input");
-	// end of while loop
-	
-	return TRUE;
 }
