f25e2c2d26de2b2d5bb71def371c4f01caeed475
Jake Willson <jakedwillson@ksu.edu>
2018-12-13 12:24:02 -0600

    Update login.c
    return ACct

--- login.c
+++ login.c
@@ -1,24 +1,5 @@
-//  login.c
-//  Authentication, initialization, encryption and file I/O
-
-#include <stdlib.h>
-#include "login.h"
-
-// Parses
-Account read_one_account(char * line)
-{
-	Account a;
-	if(line == NULL) return a;
-	const char delim[2] = "~";
-	char * token = strtok(line, delim);
-	a.username = token;
-	token = strtok(NULL, delim);
-	a.password = token;
-	return a;
-}
-
 // Evaluates equality iteratively through file
-int tryLogin(char * uname, char * psw)
+Account tryLogin(char * uname, char * psw)
 {
 	FILE * reader = fopen("AllLoginData.txt", "r");
 	Account AccArr[100];
@@ -29,18 +10,20 @@ int tryLogin(char * uname, char * psw)
 		if(strcmp(uname,AccArr[i].username) == 0 && strcmp(psw,AccArr[i].password) == 0) 
 		{
 			fclose(reader);
-			return TRUE;
+			return AccArr[i];
 		}
 	}
 	fclose(reader);
-	return FALSE;
+	Account a;
+	return a;
 }
 
 // May creates account upon successful equality testing (referential)
-int tryCreateAccount(char * uname, char * psw)
+Account tryCreateAccount(char * uname, char * psw)
 {
 	FILE * reader = fopen("AllLoginData.txt", "w");
 	Account AccArr[100];
+	Account a;
 	char line[10000];
 	for(int i = 0; fscanf(reader, "%s\n", line) != EOF; i++)
 	{
@@ -48,20 +31,13 @@ int tryCreateAccount(char * uname, char * psw)
 		if(strcmp(uname,AccArr[i].username) == 0) 
 		{
 			fclose(reader);
-			return FALSE;
+			return a;
 		}
 	}
+	Account a;
+	a.username = uname;
+	a.password = psw;
 	fprintf("%s~%s", uname, psw);
 	fclose(reader);
-	return TRUE;
-}
-
-void encrypt_one_password(char * psw)
-{
-	
-}
-
-void decrypt_one_password(char * psw)
-{
-	
+	return a;
 }
